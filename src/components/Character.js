761b95aace5b8ee1e0d90d4687f229b57d0721ad
john Caropino <jccaropino7786@live.com>
2022-12-15 14:28:35 -0800

    updated url

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,9 +1,9 @@
 
-const Character = ({id, image, name, status, species, type, gender}) => {
+const Character = ({id, image, name, status, species, type, gender, url}) => {
     return(
 <div className="Character" /*id={CHARACTER ID HERE}*/>
      <li>
-       <a href={image} target="_blank" rel="noreferrer"><h4>{name}</h4></a>
+       <a href={url} target="_blank" rel="noreferrer"><h4>{name}</h4></a>
        <img src={image} alt={name} />
        <span className="card-detail">Status: {status}</span>
        <span className="card-detail">Species: {species}</span>
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,12 @@
 import Character from "./Character"
 
 const CharacterList = ({charArray}) => {
-console.log(charArray)
+// console.log(charArray)
     // const { id, image, name, status, species, type, gender} = charArray
 
     // console.log(charArray.id)
     const characterList = charArray.map((character) => (
-        <Character key={character.id} id={character.id} image={character.image} name={character.name} status={character.status} species={character.species} type={character.type} gender={character.gender}></Character>  
+        <Character key={character.id} id={character.id} image={character.image} name={character.name} status={character.status} species={character.species} type={character.type} gender={character.gender} url={character.url} ></Character>  
 )) 
 
     return(
